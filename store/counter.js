7977b2d06fcc19c76575a5acb0ace2c493d76a9a
Martin Sagat <martin.sagat@outlook.com.au>
2021-07-09 14:51:32 +1000

    code cleanup

--- store/counter.js
+++ store/counter.js
@@ -6,22 +6,6 @@ export const state = () => ({
     value: state => state.value,
   }
   
-//   export const actions = {
-//     async fetchOffice ({ commit }, officeId) {
-//       commit('setOfficeStatus', 'loading')
-  
-//       await this.$axios.get(`/offices/${officeId}`)
-//         .then((res) => {
-//           commit('setOffice', res.data.data)
-//           commit('setOfficeStatus', 'success')
-//         })
-//         .catch((err) => {
-//           console.log(err)
-//           commit('setOfficeStatus', 'error')
-//         })
-//     },
-//   }
-  
   export const mutations = {
     setValue (state, number) {
       state.value = number
